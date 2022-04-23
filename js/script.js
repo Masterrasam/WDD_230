48d08741e993182d910dc94e4f5ece5d50494a1d
Masterrasam <103944766+Masterrasam@users.noreply.github.com>
2022-04-23 15:40:09 +0100

    assignment portal

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let nd = new Date();
+
+document.getElementById("currentYear").textContent = nd.getFullYear();
+
+let lm = document.lastModified;
+
+document.getElementById("modDate").textContent = lm;
